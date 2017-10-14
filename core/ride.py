65a3fd424705aace035043cae0210a0828b575ce
Lucie Hammond <lucie.hammond@student.ecp.fr>
2017-10-14 19:21:28 +0200

    define unser caracteristics

--- core/ride.py
+++ core/ride.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# Critères de préférences pour un trajet
+FASTEST  = "FASTEST"
+SHORTEST = "SHORTEST"
+CHEAPEST = "CHEAPEST"
+EASIEST = "EASIEST"
+NICEST = "NICEST"
+LESS_WAKING = "LESS_WALKING"
+
+
 class Ride():
 
     def __init__(self, start, end, departure_time):
--- core/user.py
+++ core/user.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-class User():
+from ride import FASTEST, SHORTEST, CHEAPEST, EASIEST, NICEST, LESS_WAKING
+from routes import bicycling, driving, transit
+
+
+class User:
 
     total_users = 0
 
@@ -10,11 +14,23 @@ class User():
         self._username = username
         self._password = password
         self._age = age
+        self._subscriptions = {"velib": bicycling.NO_SUBSCRIPTION,
+                               "autolib": driving.PRET_A_ROULER,
+                               "subway": transit.NO_RATP_TITLE}
+        self._driving_licence = True
+        self._preferences = {FASTEST: 4, SHORTEST: 0, CHEAPEST: 3, EASIEST: 2, NICEST: 5, LESS_WAKING: 1}
+
+    @property
+    def username(self):
+        return self._username
 
     def set_subscriptions_infos(self, velib, autolib, subway):
         self._subscriptions = {"velib": velib, "autolib": autolib, "subway": subway}
 
-    def set_preferences(self):
-        pass
-
-    
+    def set_preferences(self, fastest, shortest, cheapest, easiest, nicest, less_walking):
+        self._preferences = {FASTEST: fastest,
+                             SHORTEST: shortest,
+                             CHEAPEST: cheapest,
+                             EASIEST: easiest,
+                             NICEST: nicest,
+                             LESS_WAKING: less_walking}
